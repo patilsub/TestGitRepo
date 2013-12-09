a4489ec16ad073df71b8c3d10496acf2e2ae3cd6
unknown <patilsub@WL2019087.ap.tieto.com>
2013-12-09 17:24:16 +0530

    myFirst commit

---
+++ com/test/java/TestOne.java
@@ -0,0 +1,32 @@
+package com.test.java;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
+
+
+public class TestOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/*	testjava testjava = new testjava();
+		testjava.inner inner = testjava.new inner();
+		inner.srt1 = "";
+		testjava.str = "";*/
+		
+		Vector v = new Vector();
+		v.add(1);
+		v.add("two");
+		
+		System.out.println(v);
+		Vector V1 = (Vector)Collections.checkedCollection(v, Integer.class);
+		
+		V1.add("q");
+		
+	}
+
+}
---
+++ com/test/java/testjava.java
@@ -0,0 +1,24 @@
+package com.test.java;
+
+import org.tieto.java.testFirst;
+
+public class testjava {
+
+	/**
+	 * @param args
+	 */
+	String str = "str";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		testjava f = new testjava();
+		//inner in = new testjava.inner();
+		
+	}
+
+	public class inner {
+		public String srt1 = "";
+		public void prnt(){
+			//System.out.println(str);
+		}
+	}
+}
