af0fe0652caa6930c8c8a4db47ffdbcbfb1b637c
Subhash <Subhash.patil@tieto.com>
2013-12-23 17:47:04 +0530

    Main class

---
+++ com/test/java/MainClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.test.java;
+
+/**
+ * @author patilsub
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Print ");
+	}
+
+}
