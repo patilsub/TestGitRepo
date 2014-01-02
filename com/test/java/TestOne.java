59c30279096e81b0e327aa9d892836e0d716ef9d
Subhash <Subhash.patil@tieto.com>
2014-01-02 15:13:07 +0530

    changes in brnach TestGitRepoV1

--- com/test/java/TestOne.java
+++ com/test/java/TestOne.java
@@ -28,5 +28,9 @@ public class TestOne {
 		V1.add("q");
 		
 	}
+	
+	public void method1(){
+		//nothing
+	}
 
 }
--- com/test/java/TestTwo.java
+++ com/test/java/TestTwo.java
@@ -1,2 +1,11 @@
+package com.test.java;
+
 //comments line no 1
 //comments line no 2
+
+public class TestTwo{
+	
+	public void sample(){
+		System.out.println("Nothing to print");
+	}
+}
